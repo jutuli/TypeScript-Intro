c708f3a15e30ad767a4f33fd8033c6995e1adccc
Julia T <juliaturkali@gmail.com>
2024-12-20 14:16:55 +0100

    String Methods Level 1-5

--- typescript-test-tasks/src/day2-tasks/script.ts
+++ typescript-test-tasks/src/day2-tasks/script.ts
@@ -25,12 +25,26 @@ const susisStatus = "Susi is going to codingschool";
 
 const susi = susisStatus.slice(0, 4);
 const is = susisStatus.slice(5, 7);
-const isGoingToSchool = susisStatus.slice(5, 17) + susisStatus.slice(23);
+const isGoingToSchool = susisStatus.slice(5, 17) + susisStatus.slice(-6);
 const school = susisStatus.slice(23);
-const susiIsSchool = susisStatus.slice(0, 8) + susisStatus.slice(23);
+const susiIsSchool = susisStatus.slice(0, 8) + susisStatus.slice(-6);
 
 document.body.innerHTML += `<p>${susi}</p>`;
 document.body.innerHTML += `<p>${is}</p>`;
-document.body.innerHTML += `<p>${isGoingToSchool}}</p>`;
+document.body.innerHTML += `<p>${isGoingToSchool}</p>`;
 document.body.innerHTML += `<p>${school}</p>`;
 document.body.innerHTML += `<p>${susiIsSchool}</p>`;
+
+// ? Level 1-5
+const whereIsSusi = "Susi is back from codingschool";
+
+const susi2 = whereIsSusi.substring(0, 4);
+const is2 = whereIsSusi.substring(5, 7);
+const school2 = whereIsSusi.substring(24);
+const susiIsSchool2 = whereIsSusi.substring(0, 8) + whereIsSusi.substring(24);
+
+document.open();
+document.write(susi2 + " ");
+document.write(is2 + " ");
+document.write(school2 + " ");
+document.write(susiIsSchool2 + " ");
