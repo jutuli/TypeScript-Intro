6ba0515568c8550d2b5d80e2de72653b02d6f9e1
Julia T <juliaturkali@gmail.com>
2024-12-20 13:31:10 +0100

    String Methods Level 1-2

--- typescript-test-tasks/src/day2-tasks/script.ts
+++ typescript-test-tasks/src/day2-tasks/script.ts
@@ -11,3 +11,11 @@ console.log(lastName.length);
 const fullName = firstName.concat(" ", lastName);
 console.log(fullName);
 console.log(fullName.length);
+
+// ? Level 1-2
+const earthQuote =
+  "How inappropriate to call this planet Earth, when clearly it is Ocean.";
+
+console.log(earthQuote.indexOf("h"));
+console.log(earthQuote.indexOf("Earth"));
+console.log(earthQuote.indexOf("Moon"));
