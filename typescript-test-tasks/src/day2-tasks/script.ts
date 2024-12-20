cc55f2f7f58dde3c09b76bd0edcdfa93934d201c
Julia T <juliaturkali@gmail.com>
2024-12-20 13:29:19 +0100

    String Methods Level 1-1

---
+++ typescript-test-tasks/src/day2-tasks/script.ts
@@ -0,0 +1,13 @@
+// ! STRING METHODS
+
+// ? Level 1-1
+
+let firstName = "Julia";
+let lastName = "T";
+
+console.log(firstName.length);
+console.log(lastName.length);
+
+const fullName = firstName.concat(" ", lastName);
+console.log(fullName);
+console.log(fullName.length);
