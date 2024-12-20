b9c5c430028554043222a189542e37e91fe72f32
Julia T <juliaturkali@gmail.com>
2024-12-20 14:10:00 +0100

    String Methods Level 1-4

--- typescript-test-tasks/src/day2-tasks/script.ts
+++ typescript-test-tasks/src/day2-tasks/script.ts
@@ -19,3 +19,18 @@ const earthQuote =
 console.log(earthQuote.indexOf("h"));
 console.log(earthQuote.indexOf("Earth"));
 console.log(earthQuote.indexOf("Moon"));
+
+// ? Level 1-4
+const susisStatus = "Susi is going to codingschool";
+
+const susi = susisStatus.slice(0, 4);
+const is = susisStatus.slice(5, 7);
+const isGoingToSchool = susisStatus.slice(5, 17) + susisStatus.slice(23);
+const school = susisStatus.slice(23);
+const susiIsSchool = susisStatus.slice(0, 8) + susisStatus.slice(23);
+
+document.body.innerHTML += `<p>${susi}</p>`;
+document.body.innerHTML += `<p>${is}</p>`;
+document.body.innerHTML += `<p>${isGoingToSchool}}</p>`;
+document.body.innerHTML += `<p>${school}</p>`;
+document.body.innerHTML += `<p>${susiIsSchool}</p>`;
