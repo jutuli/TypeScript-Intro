3d0925b2e357e2ab9d9c9ed29fe447f53b78cbe0
Julia T <juliaturkali@gmail.com>
2024-12-20 14:39:21 +0100

    String Methods Level 1-9

--- typescript-test-tasks/src/day2-tasks/script.ts
+++ typescript-test-tasks/src/day2-tasks/script.ts
@@ -1,6 +1,6 @@
 // ! STRING METHODS
 
-// ? Level 1-1
+// - =============== Level 1-1 ================
 
 let firstName = "Julia";
 let lastName = "T";
@@ -12,7 +12,7 @@ const fullName = firstName.concat(" ", lastName);
 console.log(fullName);
 console.log(fullName.length);
 
-// ? Level 1-2
+// - =============== Level 1-2 ================
 const earthQuote =
   "How inappropriate to call this planet Earth, when clearly it is Ocean.";
 
@@ -20,7 +20,7 @@ console.log(earthQuote.indexOf("h"));
 console.log(earthQuote.indexOf("Earth"));
 console.log(earthQuote.indexOf("Moon"));
 
-// ? Level 1-4
+// - =============== Level 1-4 ================
 const susisStatus = "Susi is going to codingschool";
 
 const susi = susisStatus.slice(0, 4);
@@ -35,7 +35,7 @@ document.body.innerHTML += `<p>${isGoingToSchool}</p>`;
 document.body.innerHTML += `<p>${school}</p>`;
 document.body.innerHTML += `<p>${susiIsSchool}</p>`;
 
-// ? Level 1-5
+// - =============== Level 1-5 ================
 const whereIsSusi = "Susi is back from codingschool";
 
 const susi2 = whereIsSusi.substring(0, 4);
@@ -49,7 +49,7 @@ document.write(is2 + " ");
 document.write(school2 + " ");
 document.write(susiIsSchool2 + " ");
 
-// ? Level 1-7
+// - =============== Level 1-7 ================
 const samsStatus = "Sam is good at codingschool";
 
 const badCodingschool = samsStatus.replace("good at coding", "bad at ");
@@ -60,3 +60,53 @@ document.open();
 document.write(badCodingschool + " ");
 document.write(goodSchool + " ");
 document.write(goodTennis + " ");
+
+// - =============== Level 1-9 ================
+
+const infoAboutSam = "Sam is going to codingschool";
+const susi3 = "Susi";
+const and = "and";
+
+const samSchoolMovie = infoAboutSam
+  .slice(0, 16)
+  .concat(infoAboutSam.slice(22))
+  .concat(` ${and} `)
+  .concat("to the movie theater");
+console.log(samSchoolMovie);
+
+const samMovie = infoAboutSam.slice(0, 16).concat("the movie theater");
+console.log(samMovie);
+
+const susiSamSchool = susi3
+  .concat(` ${and} `)
+  .concat(infoAboutSam.slice(0, 3))
+  .concat(" are ")
+  .concat(infoAboutSam.slice(7, 16))
+  .concat(infoAboutSam.slice(-6));
+console.log(susiSamSchool);
+
+const susiSamGymMovie = susi3
+  .concat(` ${and} `)
+  .concat(infoAboutSam.slice(0, 3))
+  .concat(" are ")
+  .concat(infoAboutSam.slice(7, 16))
+  .concat("the gym")
+  .concat(` ${and} `)
+  .concat("to the movie theater");
+
+console.log(susiSamGymMovie);
+
+const susiSchoolMovie = susi3
+  .concat(" ")
+  .concat(infoAboutSam.slice(4, 16))
+  .concat(infoAboutSam.slice(-6))
+  .concat(` ${and} `)
+  .concat("the movie theater");
+
+console.log(susiSchoolMovie);
+
+document.body.innerHTML += `<p>${samSchoolMovie}</p>`;
+document.body.innerHTML += `<p>${samMovie}</p>`;
+document.body.innerHTML += `<p>${susiSamSchool}</p>`;
+document.body.innerHTML += `<p>${susiSamGymMovie}</p>`;
+document.body.innerHTML += `<p>${susiSchoolMovie}</p>`;
