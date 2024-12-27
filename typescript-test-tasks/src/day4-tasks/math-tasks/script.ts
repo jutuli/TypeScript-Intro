a540ad43c281fb8b5accebc63e8d761bae0628b8
Julia T <juliaturkali@gmail.com>
2024-12-27 14:20:29 +0100

    Math Tasks - Level 1_2

--- typescript-test-tasks/src/day4-tasks/math-tasks/script.ts
+++ typescript-test-tasks/src/day4-tasks/math-tasks/script.ts
@@ -3,3 +3,10 @@ console.log(Math.PI);
 const pi = Math.PI;
 const roundedPI = Math.PI.toFixed(2);
 console.log(roundedPI);
+
+// - ============================ LEVEL 1_2 =========================
+const randomNumbers: number[] = [3.14, 193.4464, 0.8596433607, -2.940629089];
+console.log(Math.round(randomNumbers[0]));
+console.log(Math.round(randomNumbers[1]));
+console.log(Math.round(randomNumbers[2]));
+console.log(Math.round(randomNumbers[3]));
