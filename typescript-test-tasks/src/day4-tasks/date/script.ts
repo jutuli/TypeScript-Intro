a1c91d8809163321eaeb294cb16b388a687f6090
Julia T <juliaturkali@gmail.com>
2024-12-27 15:04:13 +0100

    Date Tasks - Level 1_1 Refactoring

--- typescript-test-tasks/src/day4-tasks/date/script.ts
+++ typescript-test-tasks/src/day4-tasks/date/script.ts
@@ -5,20 +5,14 @@ const date2 = new Date(0);
 const date3 = new Date(31556908800);
 const date4 = new Date(86400000);
 
-const p1 = document.querySelector<HTMLParagraphElement>("p:nth-of-type(1)");
-const p2 = document.querySelector<HTMLParagraphElement>("p:nth-of-type(2)");
-const p3 = document.querySelector<HTMLParagraphElement>("p:nth-of-type(3)");
-const p4 = document.querySelector<HTMLParagraphElement>("p:nth-of-type(4)");
+const pContainer = document.querySelector<HTMLDivElement>("#container");
 
-if (p1) {
-  p1.innerText = date1.toString();
+if (pContainer) {
+  pContainer.innerHTML = `
+  <p>${date1}</p>
+  <p>${date2}</p>
+  <p>${date3}</p>
+  <p>${date4}</p>`;
 }
-if (p2) {
-  p2.innerText = date2.toString();
-}
-if (p3) {
-  p3.innerText = date3.toString();
-}
-if (p4) {
-  p4.innerText = date4.toString();
-}
+
+// - =========================
