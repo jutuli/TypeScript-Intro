60afad680b4c12180abc205853b30d607479d9d5
Julia T <juliaturkali@gmail.com>
2024-12-30 13:14:05 +0100

    Day 5 - Functions Level 1_3

--- typescript-test-tasks/src/day5-tasks/script.ts
+++ typescript-test-tasks/src/day5-tasks/script.ts
@@ -1,5 +1,7 @@
 // - ================ LEVEL 1_1 =================
 
+import { isFirstDayOfMonth } from "date-fns";
+
 function intro1(): void {
   console.log("Hello Function");
 }
@@ -13,6 +15,25 @@ const intro2 = () => {
 intro2();
 
 // - ================ LEVEL 1_3 =================
+function showPersonInfo(name: string, city: string, age: number) {
+  console.log(
+    `Hallo, mein Name ist ${name}. Ich bin ${Math.floor(
+      age
+    )} Jahre alt. Ich komme aus ${city}.`
+  );
+}
+
+showPersonInfo("Sarah", "Berlin", 27);
+
+const showPersonInfo2 = (name: string, city: string, age: number) => {
+  console.log(
+    `Hallo, mein Name ist ${name}. Ich bin ${Math.floor(
+      age
+    )} Jahre alt. Ich komme aus ${city}.`
+  );
+};
+
+showPersonInfo2("Sarah", "Berlin", 27);
 
 // - ================ LEVEL 1_4 =================
 
