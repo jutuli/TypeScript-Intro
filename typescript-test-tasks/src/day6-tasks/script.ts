8e190cb4b773277f3584443db4f6f3f28ef2fc48
Julia T <juliaturkali@gmail.com>
2025-01-02 12:12:34 +0100

    Day 6 - Arrays - Complete Task Level 1_2

--- typescript-test-tasks/src/day6-tasks/script.ts
+++ typescript-test-tasks/src/day6-tasks/script.ts
@@ -7,6 +7,15 @@ console.log(luckyNumbers);
 console.log(favoritePeople);
 
 // - ==================== LEVEL 1_2 ====================
+console.log(bucketList[0]);
+console.log(bucketList[1]);
+console.log(bucketList[2]);
+console.log(luckyNumbers[0]);
+console.log(luckyNumbers[1]);
+console.log(luckyNumbers[2]);
+console.log(favoritePeople[0]);
+console.log(favoritePeople[1]);
+console.log(favoritePeople[2]);
 
 // - ==================== LEVEL 1_3 ====================
 
