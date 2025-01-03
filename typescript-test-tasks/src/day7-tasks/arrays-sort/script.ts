c1daa499bd55e3f4f88a92f226f4d4aec6553176
Julia T <juliaturkali@gmail.com>
2025-01-03 12:34:47 +0100

    Day 7 - Arrays - Sort -  Complete Task Level 1_2

---
+++ typescript-test-tasks/src/day7-tasks/arrays-sort/script.ts
@@ -0,0 +1,25 @@
+// - ================ LEVEL 1_2 ===============
+// Sortiere die Programmiersprachen in umgekehrter alphabetischer Reihenfolge.
+
+const codingLanguages: string[] = [
+  "JavaScript",
+  "Python",
+  "Java",
+  "Ruby",
+  "PHP",
+  "C++",
+  "CSS",
+  "C#",
+  "Go",
+  "C",
+  "TypeScript",
+  "Swift",
+];
+
+function sortDescending() {
+  return codingLanguages.sort().reverse();
+}
+
+console.log(sortDescending());
+
+// - ================ LEVEL 2_1 ===============
