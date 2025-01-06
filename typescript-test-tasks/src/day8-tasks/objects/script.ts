aae7dff9047b7e574147a5c6654d9506c0c9a649
Julia T <juliaturkali@gmail.com>
2025-01-06 13:46:47 +0100

    Day 8 -Objects -  Task Level 1_6

--- typescript-test-tasks/src/day8-tasks/objects/script.ts
+++ typescript-test-tasks/src/day8-tasks/objects/script.ts
@@ -117,5 +117,70 @@ console.log(unserLager.schrank["Obere Schublade"].Ordner2);
 console.log(unserLager.schreibtisch.schublade);
 
 // - ===================== LEVEL 1_6 ======================
+// Greife auf folgende Werte dieses Objekts zu und lasse sie dir auf der Konsole ausgeben:
+// "The Beatles"
+// aus dem Objekt Pink Floyd: "Download"
+// aus dem Objekt Pink Floyd: true
+// die Releasejahre: 1971 und 1983
+// Von Metallica: "MC"
+// aus dem Objekt  Metallica das Wort: "Ride"
+// Lege ein neues Objekt vom Typ Musik an und füge es zum Array hinzu.
+
+type Musik = {
+  kunstler: string;
+  title: string;
+  release_jahr: number;
+  formate: string[];
+  gold: boolean;
+};
+
+const meineTopVier: Musik[] = [
+  {
+    kunstler: "The Beatles",
+    title: "Abbey Road",
+    release_jahr: 1969,
+    formate: ["LP", "CD", "MC", "Download"],
+    gold: true,
+  },
+  {
+    kunstler: "Pink Floyd",
+    title: "Dark Side of the Moon",
+    release_jahr: 1978,
+    formate: ["CS", "CD", "LP", "Download"],
+    gold: true,
+  },
+  {
+    kunstler: "Led Zeppelin",
+    title: "Led Zeppelin IV",
+    release_jahr: 1971,
+    formate: ["CS", "LP", "Download"],
+    gold: true,
+  },
+  {
+    kunstler: "Metallica",
+    title: "Kill 'Em All and Ride the Lightning",
+    release_jahr: 1983,
+    formate: ["LP", "CD", "MC", "Download"],
+    gold: true,
+  },
+];
+
+// "The Beatles"
+console.log(meineTopVier[0].kunstler);
+
+// aus dem Objekt Pink Floyd: "Download"
+console.log(meineTopVier[1].formate[3]);
+
+// aus dem Objekt Pink Floyd: true
+console.log(meineTopVier[1].gold);
+
+// die Releasejahre: 1971 und 1983
+console.log(meineTopVier[2].release_jahr, meineTopVier[3].release_jahr);
+
+// Von Metallica: "MC"
+console.log(meineTopVier[3].formate[2]);
+
+// aus dem Objekt  Metallica das Wort: "Ride"
+console.log(meineTopVier[3].title.slice(17, 21));
 
 // - ===================== LEVEL 2_2 ======================
