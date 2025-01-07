1523353025187f1a114a609de4be098346e9d259
Julia T <juliaturkali@gmail.com>
2025-01-07 12:52:54 +0100

    Day 9 - Set - Task Level 2_1

--- typescript-test-tasks/src/day9-tasks/set/script.ts
+++ typescript-test-tasks/src/day9-tasks/set/script.ts
@@ -28,3 +28,30 @@ starWarsCharacters.add("Leia Organa"); //-> wird nicht nochmal hinzugefügt, da 
 console.log(starWarsCharacters);
 
 //- ==================== LEVEL 2_1 =====================
+// Erstelle ein Array vom Typ String mit Marvel-Charakteren: Iron Man, Captain America, Thor, Hulk, Black Widow, Spider Man
+
+const marvelCharacters: string[] = [
+  "Iron Man",
+  "Captain America",
+  "Thor",
+  "Hulk",
+  "Black Widow",
+  "Spider Man",
+];
+
+// Füge in das Array nochmal Iron Man und Thor hinten hinzu
+
+marvelCharacters.push("Iron Man");
+marvelCharacters.push("Thor");
+
+// Lass dir den Inhalt des Arrays auf der Konsole ausgeben
+
+console.log(marvelCharacters);
+
+// Erstelle ein neues Set, dass das Array als Basis hat
+
+const singleMarvelCharacters = new Set<string>(marvelCharacters);
+
+// Lass dir den Inhalt des Sets auf der Konsole ausgeben
+
+console.log(singleMarvelCharacters);
