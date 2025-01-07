3380f530a9f7a63833b56e6b3a552b59a36799e1
Julia T <juliaturkali@gmail.com>
2025-01-07 12:48:53 +0100

    Day 9 - Set - Task Level 1_1

--- typescript-test-tasks/src/day9-tasks/set/script.ts
+++ typescript-test-tasks/src/day9-tasks/set/script.ts
@@ -1,2 +1,30 @@
 //- ==================== LEVEL 1_1 =====================
+// Erstelle ein Set, das Strings enthält → new Set
+// Es soll starWarsCharacters heißen und die Anfangswerte “Luke Skywalker”, “Darth Vader” und “Obi Wan Kenobi” beinhalten
+
+const starWarsCharacters = new Set<string>([
+  "Luke Skywalker",
+  "Darth Vader",
+  "Obi Wan Kenobi",
+]);
+
+// Lass dir den Inhalt des Sets auf der Konsole ausgeben
+console.log(starWarsCharacters);
+
+// Füge “Leia Organa” hinzu
+
+starWarsCharacters.add("Leia Organa");
+
+// Lass dir den Inhalt des Sets auf der Konsole ausgeben
+
+console.log(starWarsCharacters);
+
+// Füge erneut “Leia Organa” hinzu
+
+starWarsCharacters.add("Leia Organa"); //-> wird nicht nochmal hinzugefügt, da im Set jedes Element nur 1x enthalten sein dar
+
+// Lass dir den Inhalt des Sets auf der Konsole ausgeben
+
+console.log(starWarsCharacters);
+
 //- ==================== LEVEL 2_1 =====================
