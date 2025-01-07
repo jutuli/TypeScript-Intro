2aebbad74d4391fdd6b47fdc893e81c2c7b440b3
Julia T <juliaturkali@gmail.com>
2025-01-07 12:44:43 +0100

    Day 9 - For Of  - Task Level 1_4

--- typescript-test-tasks/src/day9-tasks/for-of/script.ts
+++ typescript-test-tasks/src/day9-tasks/for-of/script.ts
@@ -1,1 +1,23 @@
 //- ==================== LEVEL 1_4 =====================
+// Deklariere ein Array "friendNames" mit den Werten: "Georg", "Anass", "Elaine", "Hakan", "Eric", "Kim" und "Sergio"
+
+const friendNames: string[] = [
+  "Georg",
+  "Anass",
+  "Elaine",
+  "Hakan",
+  "Eric",
+  "Kim",
+  "Sergio",
+];
+
+// Dann verwende eine for-Schleife, um alle Namen in der Konsole anzuzeigen
+
+for (const name of friendNames) {
+  console.log(name);
+}
+
+// alternativ:
+friendNames.forEach((name) => console.log(name));
+
+// Verwende anschließend eine for … of - Schleife um alle Namen in der Konsole anzuzeigen
