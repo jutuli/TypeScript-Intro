76eecb609e427f431b67c8a5148a408fe42cf5ee
Julia T <juliaturkali@gmail.com>
2025-01-08 12:32:38 +0100

    Day 10 - Loops - Task Level 1_3

--- typescript-test-tasks/src/day10-tasks/script.ts
+++ typescript-test-tasks/src/day10-tasks/script.ts
@@ -23,6 +23,19 @@ for (let i = 1; i <= 10; i++) {
 console.log(numbersToTen);
 
 // - ======================= LEVEL 1_3 =========================
+// Schreibe eine while-Schleife, die "Hallo World + Nummer" 10 mal anzeigt.
+// Lege dazu eine Variable counter mit dem Wert 0 an.
+// Als Bedingung nutze in der Schleife "counter < 10".
+// Gib den String "Hallo World + counter" in der Konsole aus.
+// Erhöhe counter um eins.
+
+let counter: number = 0;
+
+while (counter < 10) {
+  console.log("This is While Loop Number ", counter + 1);
+  counter++;
+}
+
 // - ======================= LEVEL 1_7 =========================
 // - ======================= LEVEL 1_8 =========================
 // - ======================= LEVEL 2_1 =========================
