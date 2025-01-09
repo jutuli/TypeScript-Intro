87582f3368588ea396d2204626e92f2c9b704c39
Julia T <juliaturkali@gmail.com>
2025-01-09 13:31:51 +0100

    Day 11 - Loops II - Task Level 1_10

--- typescript-test-tasks/src/day11-tasks/script.ts
+++ typescript-test-tasks/src/day11-tasks/script.ts
@@ -1,4 +1,21 @@
 // - ======================= LEVEL 1_10 =========================
+//Schreibe eine Funktion getHighestNumber
+//Die Funktion bekommt als Parameter ein Array von numbers übergeben
+//Die Funktion hat als Rückgabetyp den Typ number
+function getHighestNumber(numbers: number[]): number {
+  //Es soll der höchste Wert aus dem übergebenen Array von Zahlen ermittelt werden
+  const highestNumber = Math.max(...numbers);
+  //Anschließend wird dieser Wert mit return zurückgegeben
+  return highestNumber;
+}
+
+//Rufe die Funktion mit mehreren Arrays auf, um zu testen, ob sie richtig funktioniert
+const numbers: number[] = [2, 5, 8, 2, 67, 65, 13, 33, 7, 3, 84, 13, 72];
+const moreNumbers: number[] = [45, 234, 774, 123, 976, 232];
+
+console.log(getHighestNumber(numbers));
+console.log(getHighestNumber(moreNumbers));
+
 // - ======================= LEVEL 2_3 =========================
 // - ======================= LEVEL 2_4 =========================
 // - ======================= LEVEL 3_4 =========================
