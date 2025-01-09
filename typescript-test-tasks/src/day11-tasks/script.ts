9da6a76a4e2eec29372cc4b6d9d430c44042cbc5
Julia T <juliaturkali@gmail.com>
2025-01-09 13:47:21 +0100

    Day 11 - Loops II - Task Level 2_3

--- typescript-test-tasks/src/day11-tasks/script.ts
+++ typescript-test-tasks/src/day11-tasks/script.ts
@@ -17,6 +17,24 @@ console.log(getHighestNumber(numbers));
 console.log(getHighestNumber(moreNumbers));
 
 // - ======================= LEVEL 2_3 =========================
+
+// Schreibe eine Funktion getNumberOfVowels
+function getNumberOfVowels(text: string): number {
+  // Sie soll die Anzahl der Vokale (A, E, I, O, U) in einem übergebenen String zählen und zurückgeben.
+  let NumberOfVowels = 0;
+  // Verwende eine Schleife, um jeden Buchstaben im übergebenen String zu durchzugehen, und eine Bedingung, um zu prüfen, ob es sich um einen Vokal handelt.
+  for (let i = 0; i < text.length; i++) {
+    if ("aeiou".includes(text[i].toLowerCase())) {
+      ++NumberOfVowels;
+    }
+  }
+  return NumberOfVowels;
+}
+// Teste die Funktion mit mehreren Strings
+console.log(getNumberOfVowels("Lorem ipsum dolor sit amet"));
+console.log(getNumberOfVowels("Consetetur sadipscing elitr, sed diam"));
+console.log(getNumberOfVowels("Nonumy eirmod tempor invidunt ut labore et"));
+
 // - ======================= LEVEL 2_4 =========================
 // - ======================= LEVEL 3_4 =========================
 // - ======================= LEVEL 3_5 =========================
