5db49006bb3c4836669b9a318135f5320c9865ac
Julia T <juliaturkali@gmail.com>
2025-01-15 13:26:47 +0100

    Day 15 - Module / Import & Export - Task 1_1

---
+++ typescript-test-tasks/src/day15-tasks/task1-1/app.ts
@@ -0,0 +1,9 @@
+// Importiere die Funktion aus deiner calculator.ts in der app.ts
+
+import { addFive } from "./calculator.ts";
+
+// Rufe die Funktion 3x mit beliebigen Zahlen auf
+// Die Ergebnisse sollen in der Konsole ausgegeben werden.
+console.log(addFive(4));
+console.log(addFive(3));
+console.log(addFive(10));
---
+++ typescript-test-tasks/src/day15-tasks/task1-1/calculator.ts
@@ -0,0 +1,6 @@
+// Erstelle in der calculator.ts eine Funktion namens addFive, die zu einer als Parameter übergebenen Zahl 5 dazuaddiert und das Ergebnis zurückgibt
+
+// Exportiere die Methode mit ihrem Namen
+export function addFive(num: number) {
+  return num + 5;
+}
