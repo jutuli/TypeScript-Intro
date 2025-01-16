694fdd8a2ac879c208adf58093767cac1e6c949a
Julia T <juliaturkali@gmail.com>
2025-01-16 18:01:01 +0100

    Day 16 - Flowcharts / Project: TradingCards - Clean up Comments

--- typescript-test-tasks/src/day16-tasks/app.ts
+++ typescript-test-tasks/src/day16-tasks/app.ts
@@ -2,8 +2,3 @@ import { initializeUI } from "./ui";
 
 // App initialisieren
 initializeUI();
-
-//- Verwaltung der Kartensammlung -> add, edit, delete
-// Delete: Füge bei jeder Karte die Option hinzu, sie zu löschen
-//Edit: Füge bei jeder Karte die Option hinzu, die Daten zu bearbeiten
-//für das Ändern: sollen die Daten in das Formular geladen werden
--- typescript-test-tasks/src/day16-tasks/ui.ts
+++ typescript-test-tasks/src/day16-tasks/ui.ts
@@ -2,12 +2,7 @@ import { getFormData } from "./components/formExtractor.ts";
 import { resetForm } from "./components/formExtractor.ts";
 import { createCard } from "./components/CardCreator.ts";
 
-// Interaktion des Formulars verwalten
-// Formulareingabe sammeln
-// Erstellung von Karte & Anzeigen in DOM
-// Button Add/Save/Delete -> soll der Add-Button in Save umbenannt werden, nach dem Speichern der Änderung wird das Formular wieder geleert, wird der Save-Button in Add zurückbenannt
-
-// Input aus dem Formular speichern
+// UI initialisieren und auf Button-Klick Funktionen zur Sammlung des Formularinputs und Karte erstellen aufrufen sowie Formular resetten
 export const initializeUI = () => {
   document.querySelector("#add-card-btn")?.addEventListener("click", (e) => {
     e.preventDefault();
