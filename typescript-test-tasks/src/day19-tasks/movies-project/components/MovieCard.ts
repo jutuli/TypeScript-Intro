038536d9009c04dc1938c5d9bd95b29d99dec795
Julia T <juliaturkali@gmail.com>
2025-01-21 15:22:39 +0100

    Day 19 - Project Movie DB - Implement searchMovies Fn

--- typescript-test-tasks/src/day19-tasks/movies-project/components/MovieCard.ts
+++ typescript-test-tasks/src/day19-tasks/movies-project/components/MovieCard.ts
@@ -1,21 +1,27 @@
 import movies from "../movies";
 
-const movieContainer = document.querySelector("#movie-grid");
-
-const createMovieCard = () =>
+// movies besteht aus einem Array aus Arrays [], das innere Array aus String oder String[]
+const createMovieCard = (
+  movies: [string, string, string, string, string[], string][],
+) => {
+  const movieContainer = document.querySelector("#movie-grid");
+  if (!movieContainer) return;
+  // Vorherige Inhalte entfernen, falls z.B. gefiltert wird
+  movieContainer.innerHTML = "";
   movies.forEach(([title, year, director, duration, genres, rating]) => {
     const movieCardElement = document.createElement("div");
     movieCardElement.className =
-      "flex aspect-square flex-col items-center justify-start gap-1 rounded-xl bg-lime-200 pt-10";
+      "flex aspect-square flex-col items-center justify-start gap-1 rounded-xl bg-lime-200 pt-6 px-3";
     movieCardElement.innerHTML = `
-  <h3 class="text-xl font-bold text-gray-900">${title}</h3>
-  <p class="text-base text-gray-900">${year}</p>
-  <p class="py-2 text-base font-semibold text-gray-900">${director}</p>
-  <p class="text-base text-gray-900">${duration}</p>
-  <p class="text-base text-gray-900">${genres}</p>
-  <p class="text-base text-gray-900">${rating}</p>
+  <h3 class="text-xl text-center font-bold text-gray-900">${title}</h3>
+  <p class="text-base text-center text-gray-900">${year}</p>
+  <p class="py-2 text-base text-center font-semibold text-gray-900">${director}</p>
+  <p class="text-base text-center text-gray-900">${duration}</p>
+  <p class="text-base text-center text-gray-900">${genres.join(", ")}</p>
+  <p class="text-base text-center text-gray-900">Rating: ${rating}</p>
   `;
-    movieContainer?.appendChild(movieCardElement);
+    movieContainer.appendChild(movieCardElement);
   });
+};
 
 export default createMovieCard;
--- typescript-test-tasks/src/day19-tasks/movies-project/main.ts
+++ typescript-test-tasks/src/day19-tasks/movies-project/main.ts
@@ -1,3 +1,34 @@
 import createMovieCard from "./components/MovieCard";
+import movies from "./movies";
 
-createMovieCard();
+createMovieCard(movies);
+
+function searchMovies(input: string) {
+  const matchingMovies = movies.filter((movie) => {
+    for (const element of movie) {
+      if (element.includes(input)) {
+        return true;
+      }
+    }
+    return false;
+  });
+  if (matchingMovies.length > 0) {
+    createMovieCard(matchingMovies);
+  } else {
+    const movieGrid = document.querySelector("#movie-grid");
+    if (movieGrid) {
+      movieGrid.classList.remove("grid-cols-3");
+      movieGrid.innerHTML =
+        "<p class='font-lg font-bold text-center'>Es wurde kein passender Film gefunden.</p>";
+    }
+  }
+}
+
+// Funktionsaufruf durch Button Click auf Search
+document.querySelector("#submit-btn")?.addEventListener("click", (e) => {
+  e.preventDefault();
+  // Input auslesen
+  const searchInput = document.querySelector<HTMLInputElement>("#search-input");
+  const searchInputValue = searchInput?.value || "";
+  searchMovies(searchInputValue);
+});
