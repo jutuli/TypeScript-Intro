4a64b0b4234972f860a1274e89799e0cc6a1ed48
Julia T <juliaturkali@gmail.com>
2025-01-22 12:40:21 +0100

    Day 20 - Fetch / APIs I - Task 1_1

--- typescript-test-tasks/src/day20-tasks/script.ts
+++ typescript-test-tasks/src/day20-tasks/script.ts
@@ -1,3 +1,16 @@
 // - ===================== LEVEL 1_1 =====================
+// Nutze den Endpunkt /list der [Picsum API](https://picsum.photos/v2/list) und lass dir die Liste der Bilder in der Console ausgeben.
+// Fange mögliche Fehler beim Aufruf der API ab und zeige sie in der Konsole an
+
+fetch("https://picsum.photos/v2/list")
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) =>
+    console.log(
+      "There has been an error, please check the error message: ",
+      error,
+    ),
+  );
+
 // - ===================== LEVEL 1_2 =====================
 // - ===================== LEVEL 2_1 =====================
