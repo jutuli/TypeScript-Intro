59518c56172c2c411a9b2f01803de71d05c34b03
Julia T <juliaturkali@gmail.com>
2025-01-23 14:13:16 +0100

    Day 21 - APIs II / Fetch - Task 3_1

--- typescript-test-tasks/src/day21-tasks/fetch/script.ts
+++ typescript-test-tasks/src/day21-tasks/fetch/script.ts
@@ -37,3 +37,51 @@ fetch("https://jsonplaceholder.typicode.com/todos")
   .catch((error) => console.log("There has been an error: ", error));
 
 // - =================== LEVEL 3_1 ===================
+// Suche dir eine API von Folgenden aus:
+// API um Informationen über Länder abzurufen: https://restcountries.com/
+// Pokemon API: https://pokeapi.co/
+// Rick and Morty API: [https://rickandmortyapi.com](https://rickandmortyapi.com/)
+// *Quizfragen: https://the-trivia-api.com/docs/v2/*
+// Schau zunächst in die Dokumentation und rufe Daten mit Postman oder einem anderen Tool ab
+
+// Lege ein oder mehrere Interfaces für die Daten an, die du abrufen und darstellen willst
+interface QuizQuestionModel {
+  category: string;
+  difficulty: string;
+  id: string;
+  isNiche: boolean;
+  regions: string[];
+  tags: string[];
+  type: string;
+  question: {
+    text: string;
+  };
+  correctAnswer: string;
+  incorrectAnswers: string[];
+}
+
+const quizContainer = document.querySelector("#task3-1");
+
+// Rufe die Daten ab
+fetch("https://the-trivia-api.com/v2/questions")
+  .then((response) => response.json())
+  .then((quizEntries) => {
+    console.log(quizEntries);
+    if (!quizContainer) return;
+    // Stelle die Daten ansprechend auf einer HTML-Seite dar
+    quizEntries.forEach((quizEntry: QuizQuestionModel) => {
+      const quizElement = document.createElement("article");
+      quizElement.innerHTML = `
+        <h3 class="mt-6 mb-2 text-xl font-bold">${quizEntry.question.text}</h3>
+        <ul class="grid w-1/2 grid-cols-2 gap-4">
+          <li><button class="w-full cursor-pointer rounded-2xl border border-amber-500 px-4 py-2 text-left hover:bg-amber-500 hover:text-white">A) ${quizEntry.incorrectAnswers[0]}</button></li>
+          <li><button class="w-full cursor-pointer rounded-2xl border border-amber-500 px-4 py-2 text-left hover:bg-amber-500 hover:text-white">B) ${quizEntry.incorrectAnswers[2]}</button></li>
+          <li><button class="w-full cursor-pointer rounded-2xl border border-amber-500 px-4 py-2 text-left hover:bg-amber-500 hover:text-white">C) ${quizEntry.correctAnswer}</button></li>
+          <li><button class="w-full cursor-pointer rounded-2xl border border-amber-500 px-4 py-2 text-left hover:bg-amber-500 hover:text-white">D) ${quizEntry.incorrectAnswers[1]}</button></li>
+        </ul>
+      `;
+      quizContainer.appendChild(quizElement);
+    });
+  })
+  // Fange mögliche Fehler ab
+  .catch((error) => console.log("There has been an error: ", error));
