0e7d5f64a99dbe524da69d489e2e16aab3501c58
Julia T <juliaturkali@gmail.com>
2025-02-04 14:27:09 +0100

    Day 29 - Union & Intersection Types / Error Handling - UT Level2_1

--- typescript-test-tasks/src/day29-tasks/script.ts
+++ typescript-test-tasks/src/day29-tasks/script.ts
@@ -56,6 +56,84 @@ console.log(isNumber("One"));
 console.log(isNumber(null));
 
 // - ==================== LEVEL 2_1 ==================
+// Erstelle folgende Datentypen (type)
+// Circle
+// shape: name (”circle”)
+// radius: number
+// Square
+// shape: name (”square”)
+// sideLength: number
+// Triangle
+// shape: name (”triangle”)
+// base: number
+// height: number
+// Rectangle
+// shape: name (”rectangle”)
+// width: number
+// height: number
+
+type Circle = {
+  shape: "circle";
+  radius: number;
+};
+
+type Square = {
+  shape: "square";
+  sideLength: number;
+};
+
+type Triangle = {
+  shape: "triangle";
+  base: number;
+  height: number;
+};
+
+type Rectangle = {
+  shape: "rectangle";
+  width: number;
+  height: number;
+};
+
+// Schreibe eine Funktion calculateArea, die die Fläche berechnet
+// Der Übergabeparameter soll ein UnionType von Circle, Square, Triangle,  Rectangle und undefined sein
+// der Rückgabewert ist die Größe der Fläche als number
+// Je nach Form, soll die Fläche entsprechend berechnet werden (Recherchiere nochmal, wenn du dich nicht mehr an die Formeln erinnern kannst)
+// Wird undefined übergeben, soll ein Fehler geworfen werden “No suitable shape found.”
+
+function calculateArea(
+  object: Circle | Square | Triangle | Rectangle | undefined,
+): number {
+  switch (object?.shape) {
+    case "circle":
+      return object.radius ** 2 * Math.PI;
+      break;
+    case "square":
+      return object.sideLength ** 2;
+      break;
+    case "rectangle":
+      return object.width * object.height;
+      break;
+    case "triangle":
+      return (object.base * object.height) / 2;
+    default:
+      throw new Error("The area could not be calculated.");
+  }
+}
+
+// Erstelle je ein Objekt für jede Form und übergebe sie an die Funktion calculateArea
+
+const circle: Circle = { shape: "circle", radius: 2 };
+const square: Square = { shape: "square", sideLength: 4 };
+const rectangle: Rectangle = { shape: "rectangle", width: 10, height: 5 };
+const triangle: Triangle = { shape: "triangle", base: 5, height: 3 };
+
+console.log(calculateArea(circle));
+console.log(calculateArea(square));
+console.log(calculateArea(rectangle));
+console.log(calculateArea(triangle));
+
+// Teste auf den Fall, wenn undefined übergeben wird
+console.log(calculateArea(undefined));
 
 // ! ======================= INTERSECTION TYPES ==================
 // - ==================== LEVEL 1_1 ==================
